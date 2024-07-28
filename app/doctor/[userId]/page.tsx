8efc8a5161adac1368217a670af576d78e62ce05
kanugurajesh <kanugurajesh3@gmail.com>
2024-07-28 07:27:26 +0530

    Making the final pushes

---
+++ app/doctor/[userId]/page.tsx
@@ -0,0 +1,29 @@
+"use  client"
+
+import { useEffect, useState } from 'react'
+
+export default function Page({ params }: { params: { userId: string } }) {
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    const sendResult = async () => {
+      const result = await fetch(`/api/graph`, {
+        method: 'POST',
+        body: JSON.stringify({ userId: params.userId }),
+      })
+      const data = await result.json()
+      setData(data)
+    }
+    sendResult()
+  }, [params])
+
+  useEffect(() => {
+    console.log(data)
+  }, [data])
+
+  return (
+    <main>
+      <div>My Post: {params.userId}</div>
+    </main>
+  )
+}
