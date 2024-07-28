c016b6bf3f2b6cf26e0bae386bde065a9c1dedc1
kanugurajesh <kanugurajesh3@gmail.com>
2024-07-28 18:29:35 +0530

    Adding the chagnes to the github

--- app/doctor/page.tsx
+++ app/doctor/page.tsx
@@ -30,39 +30,41 @@ const Page = () => {
 
   return (
     <main className="flex flex-col gap-2 justify-center items-center">
-      <h1 className="font-bold text-2xl mt-5">Check your patients</h1>
-      {users.length > 0 ? (
-        users.map((user: userI) => (
-          <Link
-            // @ts-ignore
-            href={`/doctor/${users[0]?.userId}`}
-            className="mt-10 flex flex-wrap gap-4"
-            key={uuid()}
-          >
-            <div
+      <h1 className="font-bold text-2xl mt-5 mb-3">Check your patients</h1>
+      <div className='flex gap-6'>
+        {users.length > 0 ? (
+          users.map((user: userI) => (
+            <Link
+              // @ts-ignore
+              href={`/doctor/${user.userId}`}
+              className="mt-10 flex flex-wrap gap-4"
               key={uuid()}
-              className="p-4 bg-blue-500 text-white w-fit flex flex-col justify-center items-center gap-4 rounded-md border-2 border-blue-500 hover:bg-white hover:text-blue-500 transition-all ease-in-out duration-300 cursor-pointer"
             >
-              <Image
-                src={user.imageUrl}
-                alt={user.name}
-                width={150}
-                height={150}
-                className="rounded-full"
-              />
-              <div className="font-bold flex flex-col items-center justify-center gap-2">
-                <div className="group">
-                  <p>{user.name}</p>
-                  <div className="w-0 group-hover:w-full bg-black h-1 transition-all ease-in-out duration-300"></div>
+              <div
+                key={uuid()}
+                className="p-4 bg-blue-500 text-white w-fit flex flex-col justify-center items-center gap-4 rounded-md border-2 border-blue-500 hover:bg-white hover:text-blue-500 transition-all ease-in-out duration-300 cursor-pointer"
+              >
+                <Image
+                  src={user.imageUrl}
+                  alt={user.name}
+                  width={150}
+                  height={150}
+                  className="rounded-full"
+                />
+                <div className="font-bold flex flex-col items-center justify-center gap-2">
+                  <div className="group">
+                    <p>{user.name}</p>
+                    <div className="w-0 group-hover:w-full bg-black h-1 transition-all ease-in-out duration-300"></div>
+                  </div>
+                  <p>{user.email}</p>
                 </div>
-                <p>{user.email}</p>
               </div>
-            </div>
-          </Link>
-        ))
-      ) : (
-        <p>No users found</p>
-      )}
+            </Link>
+          ))
+        ) : (
+          <p>No users found</p>
+        )}
+      </div>
     </main>
   )
 }
