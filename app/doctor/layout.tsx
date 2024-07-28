fea6147c492b42c146a97cb1409d0b3586895d2b
kanugurajesh <kanugurajesh3@gmail.com>
2024-07-28 19:44:03 +0530

    Adding hover effect to the object

--- app/doctor/layout.tsx
+++ app/doctor/layout.tsx
@@ -61,7 +61,7 @@ export default function RootLayout({
           <div className="flex items-center justify-between">
             <button
               type="submit"
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
             >
               Login
             </button>
